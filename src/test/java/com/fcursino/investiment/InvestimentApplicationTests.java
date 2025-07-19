39eff8616152c0b074e8fafc657ebf89ef5dd98e
fabricio.cursino <fabricio.cursino@escoladnc.com.br>
2025-07-19 15:58:41 -0300

    setting maven project base

---
+++ src/test/java/com/fcursino/investiment/InvestimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcursino.investiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
