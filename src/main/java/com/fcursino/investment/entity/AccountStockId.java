55e366a5bf7fa65fa6d4b7757afce40c746fe69a
fabricio.cursino <fabricio.cursino@escoladnc.com.br>
2025-07-23 10:59:49 -0300

    removing unused package imports

--- src/main/java/com/fcursino/investment/entity/AccountStockId.java
+++ src/main/java/com/fcursino/investment/entity/AccountStockId.java
@@ -1,11 +1,9 @@
 package com.fcursino.investment.entity;
 
-import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
