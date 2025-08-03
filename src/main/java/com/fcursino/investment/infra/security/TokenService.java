0ceedc13b1b8783e74f61473c2ef288d18c38589
fcursino <fabriciocursino09@gmail.com>
2025-08-03 16:12:02 -0300

    adding spring security config to generate and authenticate user tokens

---
+++ src/main/java/com/fcursino/investment/infra/security/TokenService.java
@@ -0,0 +1,53 @@
+package com.fcursino.investment.infra.security;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.fcursino.investment.entity.User;
+
+@Service
+public class TokenService {
+  @Autowired
+  @Value("${jwt.secret}")
+  private String secret;
+  public String generateToken(User user) {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(secret);
+
+      String token = JWT.create()
+          .withIssuer("investment")
+          .withSubject(user.getEmail())
+          .withExpiresAt(this.generateExpirationDate())
+          .sign(algorithm);
+      return token;
+    } catch (JWTCreationException exception) {
+      throw new RuntimeException("Error while authenticating");
+    }
+  }
+
+  public String validateToken(String token) {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(secret);
+      return JWT.require(algorithm)
+          .withIssuer("investment")
+          .build()
+          .verify(token)
+          .getSubject();
+    } catch (JWTVerificationException exception) {
+      return null;
+    }
+  }
+
+  private Instant generateExpirationDate() {
+    return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+  }
+}
