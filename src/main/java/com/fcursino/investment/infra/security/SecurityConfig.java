fe89fd8f368ac39d4924501ffe8e3bcdb907cc58
fcursino <fabriciocursino09@gmail.com>
2025-08-04 13:36:20 -0300

    validating dispatcher type of requests in security config to prevent generic response status

--- src/main/java/com/fcursino/investment/infra/security/SecurityConfig.java
+++ src/main/java/com/fcursino/investment/infra/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import jakarta.servlet.DispatcherType;
 
 @Configuration
 @EnableWebSecurity
@@ -29,8 +30,9 @@ public class SecurityConfig {
           .cors(Customizer.withDefaults())
           .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
           .authorizeHttpRequests(authorize -> authorize
-              .requestMatchers(HttpMethod.POST, "/v1/auth/login").permitAll()
-              .requestMatchers(HttpMethod.POST, "/v1/auth/register").permitAll()
+              .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
+              .requestMatchers(HttpMethod.POST, "/v1/users/auth").permitAll()
+              .requestMatchers(HttpMethod.POST, "/v1/users").permitAll()
               .anyRequest().authenticated()
           )
           .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
